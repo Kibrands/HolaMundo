9bce1d2836f336c8d19301244e58780837e783ec
Kibrands <jjextreme13@gmail.com>
2019-04-01 12:01:54 +0200

    Initial code added

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package holamundo;
+
+/**
+ *
+ * @author Julio de la Matta
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Ejemplo para pruebas");
+    }
+
+}
